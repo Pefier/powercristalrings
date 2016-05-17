a92b5d44842accb9b8b86b4040969c34f65033ce
Pefier <pefier@hotmail.com>
2016-05-17 20:06:07 +0200

    Fixed bug

--- src/main/java/com/pefier/powercristalrings/crafting/VanillaRecepies.java
+++ src/main/java/com/pefier/powercristalrings/crafting/VanillaRecepies.java
@@ -3,6 +3,7 @@ package com.pefier.powercristalrings.crafting;
 import com.pefier.powercristalrings.init.ModBlocks;
 import com.pefier.powercristalrings.init.ModItems;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
@@ -16,6 +17,7 @@ public class VanillaRecepies {
         GameRegistry.addRecipe( new ItemStack(ModBlocks.cristallForge) ,"ggg","cfc","ooo",'o', Blocks.obsidian,'g',Blocks.glass,'c', new ItemStack(ModItems.powerCristall,1, OreDictionary.WILDCARD_VALUE), 'f',Blocks.furnace);
         GameRegistry.addRecipe(new ItemStack(ModBlocks.charger),"o o","oco","ooo", 'o',Blocks.obsidian,'c',new ItemStack(ModItems.powerCristallUnlocked,1, OreDictionary.WILDCARD_VALUE));
         GameRegistry.addRecipe(new ItemStack(ModBlocks.craftingSurface),"oco","ctc","oco",'o',Blocks.obsidian,'c',new ItemStack(ModItems.powerCristallUnlocked,1, OreDictionary.WILDCARD_VALUE),'t',Blocks.crafting_table);
+        GameRegistry.addRecipe(new ItemStack(ModItems.cristallPearl),"cec","epe","cec",'c',new ItemStack(ModItems.powerCristallUnlocked,1, OreDictionary.WILDCARD_VALUE),'e', Items.ender_eye,'p',Items.ender_pearl);
 
     }
 }
